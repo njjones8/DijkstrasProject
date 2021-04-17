7233953aef3703f744e9cdf54573ec73ec47c5e9
Nicholas Jones <njjones8@sundevils.asu.edu>
2021-04-16 23:59:28 -0700

    added getters/setters

--- Project3/vertex.cpp
+++ Project3/vertex.cpp
@@ -7,12 +7,64 @@ Vertex::Vertex()
 	pi = 0;
 	distance = 0;
 	position = 0;
+	name = "";
 }
 
-Vertex::Vertex(int c, int p, float d, int pos)
+Vertex::Vertex(int c, int p, float d, int pos, std::string n)
 {
 	color = c;
 	pi = p;
 	distance = d;
 	position = pos;
+	name = n;
+}
+
+void Vertex::setColor(int x)
+{
+	color = x;
+}
+
+void Vertex::setPi(int x)
+{
+	pi = x;
+}
+
+void Vertex::setPosition(int x)
+{
+	position = x;
+}
+
+void Vertex::setDistance(float x)
+{
+	distance = x;
+}
+
+void Vertex::setName(std::string x)
+{
+	name = x;
+}
+
+int Vertex::getColor()
+{
+	return color;
+}
+
+int Vertex::getPi()
+{
+	return pi;
+}
+
+int Vertex::getPosition()
+{
+	return position;
+}
+
+float Vertex::getDistance()
+{
+	return distance;
+}
+
+std::string Vertex::getName()
+{
+	return std::string();
 }
--- Project3/vertex.h
+++ Project3/vertex.h
@@ -1,14 +1,29 @@
 #ifndef _vertex_h
 #define _vertex_h 1
 
+#include <string>
+
 class Vertex
 {
 private:
 	int color, pi, position;
 	float distance;
+	std::string name;
 public:
 	Vertex();
-	Vertex(int, int, float, int);
+	Vertex(int, int, float, int, std::string);
+
+	void setColor(int);
+	void setPi(int);
+	void setPosition(int);
+	void setDistance(float);
+	void setName(std::string);
+
+	int getColor();
+	int getPi();
+	int getPosition();
+	float getDistance();
+	std::string getName();
 };
 
 #endif
